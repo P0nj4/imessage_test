d55e0dcfb12365a061a05e2a3c068f5717108f8f
P0nj4 <german.f.pereyra@gmail.com>
2016-09-22 14:16:20 -0300

    aaa

--- Extension/DrawerViewController.swift
+++ Extension/DrawerViewController.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 import jot
+import FontAwesomeKit
+import Neon
 
 class DrawerViewController: UIViewController {
     
     let myDrawableSpace = JotViewController()
-    let changeModeButton = UIButton()
-    let addBackgroundImage = UIButton()
+    let changeMode = UIButton()
+    let pickBackground = UIButton()
+    let background = UIImageView()
     
     var isWritingModeEnabled: Bool = false {
         didSet {
@@ -32,14 +35,68 @@ class DrawerViewController: UIViewController {
         self.addChildViewController(self.myDrawableSpace)
         self.view.addSubview(self.myDrawableSpace.view)
         self.myDrawableSpace.didMove(toParentViewController: self)
-        self.myDrawableSpace.view.backgroundColor = UIColor.yellow
+        self.myDrawableSpace.view.backgroundColor = UIColor.clear
         self.myDrawableSpace.state = JotViewState.drawing
+        self.myDrawableSpace.renderImage()
         
+        
+        let changeModeImage = UIImage(stackedIcons: [FAKFontAwesome.pencilIcon(withSize: 35)], imageSize: CGSize(width: 60, height: 60))
+//        self.changeMode.setImage(changeModeImage, for: .normal)
+        self.view.addSubview(self.changeMode)
+        self.changeMode.addTarget(self, action: #selector(changeModePressed), for: .touchUpInside)
+        
+//        let pickBackgroundImage = UIImage(stackedIcons: [FAKFontAwesome.imageIcon(withSize: 35)], imageSize: CGSize(width: 60, height: 60))
+//        self.pickBackground.setImage(pickBackgroundImage, for: .normal)
+        self.view.addSubview(self.pickBackground)
+        self.pickBackground.addTarget(self, action: #selector(pickAbackgroundPressed), for: .touchUpInside)
+        
+        self.view.addSubview(self.background)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
         self.myDrawableSpace.view.frame = self.view.bounds
+        self.pickBackground.anchorInCorner(.bottomLeft, xPad: 10, yPad: 10, width: 60, height: 60)
+        self.changeMode.anchorInCorner(.bottomRight, xPad: 10, yPad: 10, width: 60, height: 60)
+        self.background.fillSuperview()
+    }
+    
+    //MARK - Actions
+    
+    func changeModePressed() {
+        
+//        
+//        var size = CGSize(width: 300, height: 300)
+//        UIGraphicsBeginImageContext(size)
+//        
+//        let areaSize = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+//        bottomImage!.drawInRect(areaSize)
+//        
+//        topImage!.drawInRect(areaSize, blendMode: kCGBlendModeNormal, alpha: 0.8)
+//        
+//        var newImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()
+//        UIGraphicsEndImageContext()
+//        
+        
+        self.isWritingModeEnabled = !self.isWritingModeEnabled
+    }
+    
+    func pickAbackgroundPressed() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+}
+
+extension DrawerViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    private func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            self.background.contentMode = .scaleAspectFit
+            self.background.image = pickedImage
+        }
+        dismiss(animated: true, completion: nil)
     }
 }
--- Podfile
+++ Podfile
@@ -4,10 +4,25 @@ platform :ios, '10.0'
 target 'Extension' do
   use_frameworks!
   pod 'jot'
+  pod 'Neon'
+  pod 'FontAwesomeKit', '~> 2.2.0'
+  pod 'FontAwesome.swift'
 end
 
 target 'WriteAndSendOnMessage' do
   # Comment this line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
   pod 'jot'
+  pod 'Neon'
+  pod 'FontAwesomeKit', '~> 2.2.0'
+  pod 'FontAwesome.swift'
 end
+
+#post_install do |installer|
+#    installer.pods_project.targets.each do |target|
+#        target.build_configurations.each do |config|
+#            config.build_settings['SWIFT_VERSION'] = '2.3'
+#        end
+#    end
+#end
+#
